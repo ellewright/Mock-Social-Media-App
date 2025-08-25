6e0adc0fee8f89066b73b0b3077f822459c19930
Eleanor Wright <elliewright055@gmail.com>
2025-08-25 15:58:36 -0500

    Update api/albums.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+// import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // const [albums, setAlbums] = useState([]);
+
+  // useEffect(() => {
+  //   async function loadData() {
+  //     const retrievedData = await getAllAlbums();
+  //     setAlbums(retrievedData);
+  //   }
+
+  //   loadData();
+  // }, []);
+
+  // console.log(albums);
+
   return (
     <></>
   );
--- src/api/albums.js
+++ src/api/albums.js
@@ -1,13 +1,28 @@
 const BASE_URL = `${import.meta.env.VITE_API_URL}/albums`;
 
-export function getAllAlbums() {
-    fetch(BASE_URL)
-        .then(response => response.json())
-        .then(json => console.log(json));
+export async function getAllAlbums() {
+    try {
+        const response = await fetch(BASE_URL);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        return response.json();
+    } catch (error) {
+        console.error(`Error fetching all albums: ${error.message}`);
+    }
+
 }
 
-export function getAlbum(albumId) {
-    fetch(`${BASE_URL}/${albumId}`)
-        .then(response => response.json())
-        .then(json => console.log(json));
+export async function getAlbum(albumId) {
+    try {
+        const response = await fetch(`${BASE_URL}/${albumId}`);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        return response.json();
+    } catch (error) {
+        console.error(`Error fetching album with ID ${albumId}: ${error.message}`);
+    }
 }
