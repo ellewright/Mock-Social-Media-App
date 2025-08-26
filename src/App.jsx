c6f6c51a692d8718b7f53839f6c982a474be3916
Eleanor Wright <elliewright055@gmail.com>
2025-08-26 13:15:43 -0500

    Create User component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css';
+import User from './components/User';
 
 function App() {
   return (
-    <></>
+    <>
+      <User userId={1} />
+    </>
   );
 }
 
---
+++ src/components/User.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { getUser } from "../api/users";
+
+export default function User({ userId }) {
+    const [user, setUser] = useState({
+        id: null,
+        name: "",
+        username: "",
+        email: "",
+        address: {
+            street: "",
+            suite: "",
+            city: "",
+            zipcode: "",
+            geo: {
+                lat: 0.0,
+                lng: 0.0
+            }
+        }
+    });
+
+    useEffect(() => {
+        const loadUser = async () => {
+            const response = await getUser(userId);
+            setUser(response);
+        };
+
+        loadUser();
+    }, [userId]);
+
+    return user.id !== null && (
+        <div>
+            <h1>{user.name}</h1>
+            <p>{user.username}</p>
+            <p>{user.email}</p>
+            <div>
+                <p>{user.address.street}</p>
+                <p>{user.address.suite}</p>
+                <p>{user.address.city}</p>
+                <p>{user.address.zipcode}</p>
+                <div>
+                    <p>{user.address.geo.lat}</p>
+                    <p>{user.address.geo.lng}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
