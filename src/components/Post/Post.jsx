b9cf12e658e9896c1284d71ea9fea642b1b0bd56
Eleanor Wright <elliewright055@gmail.com>
2025-08-26 14:05:19 -0500

    Create Post.jsx

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import styles from "./Post.module.css";
+import { getPost } from "../../api/posts";
+
+export default function Post({ postId }) {
+    const [post, setPost] = useState({
+        id: null,
+        userId: null,
+        title: "",
+        body: ""
+    });
+
+    useEffect(() => {
+        const loadPost = async () => {
+            const response = await getPost(postId);
+            setPost(response);
+        };
+
+        loadPost();
+    }, [postId]);
+
+    return (
+        <div className={styles.postContainer}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+        </div>
+    );
+}
